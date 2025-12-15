d3971ee33bc506c8cb0baa405c8424d9e5ab3b8b
dataw1 <grzegorz.twa@wp.pl>
2025-12-15 22:24:59 +0100

    Test alokacji

--- main.cpp
+++ main.cpp
@@ -1,17 +1,10 @@
 #include "matrix.h"
 #include <iostream>
-#include <ctime>
 
 using namespace std;
 
 int main() {
-    srand(time(NULL));
-
-    //Test 1 Alokacja i losowanie (n=5)
-    cout << "Test 1: Alokacja i losowanie (n=5)" << endl;
-    matrix A(5);
-    A.losuj();
-    cout << "Pierwsza macierz " << A << endl;
-    
+    matrix m(5);
+    cout << "Test alokacji pamięci" << endl;
     return 0;
 }
