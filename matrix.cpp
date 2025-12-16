e2099f4d63ec6a3d1c30342fff56aeb59231812e
MWolf <xreod999@gmail.com>
2025-12-16 16:02:17 +0100

    konstruktory kopiujace i wypelniajace w pliku z kodem

--- matrix.cpp
+++ matrix.cpp
@@ -18,4 +18,15 @@ matrix& matrix::alokuj(int req_n) {
     // Opcjonalne zerowanie
     if(allocated_n == req_n) { for(int i=0; i<n*n; ++i) data[i] = 0; }
     return *this;
+    
+}
+matrix::matrix(int n, int* t) : matrix(n) {
+    for (int i = 0; i < n * n; ++i) data[i] = t[i];
+}
+
+matrix::matrix(const matrix& m) : n(m.n), allocated_n(m.allocated_n) {
+    if (m.data) {
+        data = std::make_unique<int[]>(allocated_n * allocated_n);
+        for (int i = 0; i < n * n; ++i) data[i] = m.data[i];
+    }
 }
