5bde5b172872e81dee4e40db419be0ce29ff59af
Michał Wilk <xreod999@gmail.com>
2025-12-20 22:48:09 +0100

    dodanie 3 peirwszych testów do maina

--- main.cpp
+++ main.cpp
@@ -4,8 +4,21 @@
 using namespace std;
 
 int main() {
-    matrix m(5);
-    cout << "Test alokacji pamięci" << endl;
-    return 0;
+     srand(time(NULL));
+
+    std::cout << "--- Test 1: Alokacja i losowanie (n=5) ---" << std::endl;
+    matrix A(5);
+    A.losuj();
+    std::cout << A << std::endl;
+
+    std::cout << "--- Test 2: Diagonalna z przesunieciem (n=5, k=1) ---" << std::endl;
+    int dane[] = {1, 1, 1, 1, 1};
+    A.diagonalna_k(1, dane);
+    std::cout << A << std::endl;
+
+    std::cout << "--- Test 3: Szachownica (n=4) ---" << std::endl;
+    matrix B(4);
+    B.szachownica();
+    std::cout << B << std::endl;
     
 }
